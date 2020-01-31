62677cbb83791ef09783d81f0f1badf6ba225e34
Dominik Beens <dominik.beens@hotmail.nl>
2020-01-31 18:38:18 +0100

    lookat mouse

---
+++ GlobalGameJam2020/Assets/Scripts/LookAtMouse.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour {
+
+    private void Update() {
+        Vector3 toMouse = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(toMouse.y, toMouse.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
