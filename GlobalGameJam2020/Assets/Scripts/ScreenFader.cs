3aa4d2bc8abb161e5504f45987569df15ba2f69f
Dominik Beens <dominik.beens@hotmail.nl>
2020-01-31 23:31:47 +0100

    added scene flow and screen fader

---
+++ GlobalGameJam2020/Assets/Scripts/ScreenFader.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using DG.Tweening;
+using System;
+
+public class ScreenFader : MonoBehaviour {
+
+    public static ScreenFader Instance { get; private set; }
+
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private bool fadeOutOnAwake = true;
+
+    private bool fading;
+
+    private void Awake() {
+        Instance = this;
+
+        if (fadeOutOnAwake) {
+            canvasGroup.DOFade(1, 0);
+            FadeOut();
+        } else {
+            FadeOut(0);
+        }
+    }
+
+    public void FadeIn(float speed = 1f, Action onComplete = null) {
+        Fade(1, speed, onComplete);
+    }
+
+    public void FadeOut(float speed = 1f, Action onComplete = null) {
+        Fade(0, speed, onComplete);
+    }
+
+    private void Fade(float alpha, float speed = 1f, Action onComplete = null) {
+        if (fading) { return; }
+        fading = true;
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.DOFade(alpha, speed).OnComplete(() => {
+            onComplete?.Invoke();
+            fading = false;
+            canvasGroup.blocksRaycasts = false;
+        });
+    }
+}
