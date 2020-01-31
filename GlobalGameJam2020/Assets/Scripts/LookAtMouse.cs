dc100006c8755f753fe19d34d239121dd60dfb81
Dominik Beens <dominik.beens@hotmail.nl>
2020-01-31 19:09:00 +0100

    added world camera

--- GlobalGameJam2020/Assets/Scripts/LookAtMouse.cs
+++ GlobalGameJam2020/Assets/Scripts/LookAtMouse.cs
@@ -2,9 +2,12 @@
 
 public class LookAtMouse : MonoBehaviour {
 
+    [SerializeField] private float speed = 20f;
+
     private void Update() {
         Vector3 toMouse = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-        float angle = Mathf.Atan2(toMouse.y, toMouse.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        float angle = (Mathf.Atan2(toMouse.y, toMouse.x) * Mathf.Rad2Deg) - 90;
+        Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * speed);
     }
 }
---
+++ GlobalGameJam2020/Assets/Scripts/WorldCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class WorldCamera : MonoBehaviour {
+
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float scale = 1f;
+
+    private CinemachineCameraOffset cameraOffset;
+
+    private Camera mainCam;
+
+    private void Awake() {
+        cameraOffset = GetComponentInChildren<CinemachineCameraOffset>();
+        mainCam = Camera.main;
+    }
+
+    private void Update() {
+        Vector3 offset = mainCam.ScreenToViewportPoint(Input.mousePosition);
+        Vector3 centeredOffset = offset - new Vector3(0.5f, 0.5f, 0f);
+        centeredOffset = new Vector3(centeredOffset.x * scale, centeredOffset.y * scale, centeredOffset.z * scale);
+        cameraOffset.m_Offset = Vector3.Lerp(cameraOffset.m_Offset, centeredOffset, Time.deltaTime * speed);
+    }
+}
