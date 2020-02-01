60ea9782b58cac15b6ad52c2806390047711849e
Dominik Beens <dominik.beens@hotmail.nl>
2020-02-01 16:31:59 +0100

    added health shine

--- GlobalGameJam2020/Assets/Scripts/HealthIcon.cs
+++ GlobalGameJam2020/Assets/Scripts/HealthIcon.cs
@@ -6,6 +6,7 @@ public class HealthIcon : MonoBehaviour {
 
     [SerializeField] private Image healthImage;
     [SerializeField] private int spriteId;
+    [SerializeField] private Transform shine;
 
     public bool IsActive { get; private set; }
     public int SpriteId => spriteId;
@@ -14,11 +15,18 @@ public class HealthIcon : MonoBehaviour {
         IsActive = true;
         healthImage.DOKill();
         healthImage.DOFade(1, 0.2f);
+        shine.DOKill();
+        shine.DOScale(1, 0.2f);
+        shine.DOBlendableRotateBy(new Vector3(0, 0, -360), 1f, RotateMode.WorldAxisAdd).OnComplete(() => { 
+            shine.DOScale(0, 0.2f);
+        });
     }
 
     public void SetHealthIconOff() {
         IsActive = false;
         healthImage.DOKill();
         healthImage.DOFade(0.2f, 0.2f);
+        shine.DOKill();
+        shine.DOScale(0, 0);
     }
 }
