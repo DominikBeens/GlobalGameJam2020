3c56324969563b7bd869a099a3f02548955c3d98
Dominik Beens <dominik.beens@hotmail.nl>
2020-02-01 17:07:00 +0100

    hmm

--- GlobalGameJam2020/Assets/Scripts/MainMenu.cs
+++ GlobalGameJam2020/Assets/Scripts/MainMenu.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using DG.Tweening;
 
 public class MainMenu : MonoBehaviour {
 
     private const string GAME_SCENE_NAME = "Game";
 
     [SerializeField] private Button playButton;
+    [SerializeField] private Transform spaceShip;
 
     private void Awake() {
         playButton.onClick.AddListener(PlayGame);
@@ -17,6 +20,21 @@ public class MainMenu : MonoBehaviour {
     }
 
     private void PlayGame() {
+        StartCoroutine(StartGameRoutine());
+    }
+
+    private IEnumerator StartGameRoutine() {
+        playButton.interactable = false;
+
+        spaceShip.DOShakePosition(20f, 5f, 20).SetEase(Ease.Linear);
+        spaceShip.DOShakeScale(1f, 0.1f).SetEase(Ease.Linear);
+
+        yield return new WaitForSeconds(1f);
+
+        spaceShip.DOMove(spaceShip.position + (spaceShip.up * 7500), 5f).SetEase(Ease.InSine);
+
+        yield return new WaitForSeconds(0.5f);
+
         ScreenFader.Instance.FadeIn(1, () => {
             SceneManager.LoadScene(GAME_SCENE_NAME);
         });
